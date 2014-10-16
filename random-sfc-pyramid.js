eed2e236c00a7b66ee318a2bf1b4f33fc86d98c8
sol-sun <suzuki@g-language.org>
2014-10-16 19:52:35 +0900

    1段から100段までのSFCピラミッドをランダムに出力する関数

---
+++ random-sfc-pyramid.js
@@ -0,0 +1,28 @@
+#!/usr/local/bin/env node
+
+
+
+var randomSFCPyramid = function(){
+// you can get it with npm.. => $ npm install mersenne
+var r  = require('mersenne');
+var r_num = r.rand(100) + 1;
+
+// or use defaults (Math.rand)
+//var hoge = 1 + parseInt(Math.random() * 100);
+
+    if (　isNaN(r_num) ){
+	return;
+    }
+    
+    var join = "";
+
+    for(var i=0;i<r_num;i++){
+        join += "SFC ";
+        process.stdout.write(join);
+        process.stdout.write("\n");
+    }
+
+    
+}
+
+randomSFCPyramid();
